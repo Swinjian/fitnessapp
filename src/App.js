5c40aa8f3790ed846abba50d8121d936f784469a
Sukrutha Raj <swinwillwin4@gmail.com>
2024-04-11 15:46:03 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Routes ,Route} from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Workout from "./Components/Workout";
+import Nutrition from "./Components/Nutrition";
+import Challanges from "./Components/Challanges";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Error from "./Error";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App=()=>{
+  return(
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/workout" element={<Workout/>}/>
+      <Route path="/nutrition" element={<Nutrition/>}/>
+      <Route path="/challanges" element={<Challanges/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="*" element={<Error/>}/>
+    </Routes>
+    </>
+  )
 }
 
 export default App;
